14ae7fad2c209e2bbbf8ebcd70f0cb92bfbd5e38
SANG-MIN PARK <sanggoe0509@gmail.com>
2020-07-21 23:39:19 +0900

    chap01 study

---
+++ chap_01/check05.py
@@ -0,0 +1,7 @@
+# hello_coding / HelloCoding
+# hello_python / HelloPython
+# we_are_the_world / WeAreTheWorld
+# create_output / CreateOutput
+# create_request / CreateRequest
+# init_server / InitServer
+# init_matrix / InitMatrix
